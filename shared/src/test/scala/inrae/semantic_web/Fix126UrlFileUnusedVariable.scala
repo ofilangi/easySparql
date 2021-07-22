a1071e03ffe064bc6921be9534f977ff43d5944d
Olivier Filangi <olivier.filangi@gmail.com>
2021-07-22 22:08:34 +0200

    add test file

--- shared/src/test/scala/inrae/semantic_web/Fix126UrlFileUnusedVariable.scala
+++ shared/src/test/scala/inrae/semantic_web/Fix126UrlFileUnusedVariable.scala
@@ -1,5 +1,33 @@
 package inrae.semantic_web
 
-class Fix126UrlFileUnusedVariable {
+import utest.{TestSuite, Tests, test}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Fix126UrlFileUnusedVariable extends TestSuite {
+  val config: StatementConfiguration = StatementConfiguration.setConfigString(
+    s"""
+        {
+         "sources" : [{
+           "id"       : "file_turtle",
+           "url"      : "http://localhost:8080/animals.ttl"
+         }],
+         }
+        """.stripMargin)
+
+  def tests = Tests {
+    test("order by") {
+        SWDiscovery(config)
+          .prefix("ns0", "http://www.some-ficticious-zoo.com/rdf#")
+          .something("animal")
+          .datatype("ns0:name","name")
+          .select(Seq("animal","name"))
+          .commit()
+          .raw.map(r => {
+            println(r)
+          }
+        )
+    }
+  }
 }
+
