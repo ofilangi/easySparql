a0dd5ce8e70eaa373e00abba0c572fd78fa0d5b3
Olivier Filangi <olivier.filangi@gmail.com>
2022-05-17 16:32:25 +0200

    refactor: improve error handler from RDF4J request

--- jvm/src/main/scala/fr/inrae/metabohub/semantic_web/driver/Rdf4jRequestDriver.scala
+++ jvm/src/main/scala/fr/inrae/metabohub/semantic_web/driver/Rdf4jRequestDriver.scala
@@ -3,8 +3,9 @@ package fr.inrae.metabohub.semantic_web.driver
 import fr.inrae.metabohub.semantic_web.exception._
 import fr.inrae.metabohub.semantic_web.event.{DiscoveryRequestEvent, DiscoveryStateRequestEvent}
 import fr.inrae.metabohub.semantic_web.sparql.QueryResult
+import org.eclipse.rdf4j.query.{MalformedQueryException, UnsupportedQueryLanguageException}
 import org.eclipse.rdf4j.query.resultio.sparqljson.SPARQLResultsJSONWriter
-import org.eclipse.rdf4j.repository.RepositoryConnection
+import org.eclipse.rdf4j.repository.{RepositoryConnection, RepositoryException}
 
 import java.io.ByteArrayOutputStream
 import scala.concurrent.Future
@@ -18,26 +19,31 @@ trait Rdf4jRequestDriver extends RequestDriver {
       publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.START_HTTP_REQUEST))
       publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.PROCESS_HTTP_REQUEST))
       val out = new ByteArrayOutputStream()
-      /*
-              val result = repo.getConnection()
-                .prepareTupleQuery(query).evaluate()
-              while (result.hasNext()) println(result.next())
-      */
+
       Try(con
         .prepareTupleQuery(query)
         .evaluate(new SPARQLResultsJSONWriter(out))) match {
-        case Success(_) => {
+        case Success(_) =>
           publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.FINISHED_HTTP_REQUEST))
           publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.RESULTS_BUILD))
           val response = fr.inrae.metabohub.semantic_web.sparql.QueryResult(out.toString())
           publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.RESULTS_DONE))
           response
-        }
-        case Failure(e) => {
+        case Failure(e : MalformedQueryException) =>
           publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.ERROR_HTTP_REQUEST))
-          throw SWDiscoveryException("\n"+query+"\n"+e.getMessage)
-        }
-      }
+          throw SWDiscoveryException(s"** MalformedQueryException **\n"+query+"\n"+e.getMessage)
+        case Failure(e : UnsupportedQueryLanguageException) =>
+          publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.ERROR_HTTP_REQUEST))
+          throw SWDiscoveryException(s"** UnsupportedQueryLanguageException **\n"+e.getMessage)
+        case Failure(e : UnsupportedOperationException) =>
+          publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.ERROR_HTTP_REQUEST))
+          throw SWDiscoveryException(s"** UnsupportedOperationException **"+"\n"+e.getMessage)
+        case Failure(e : RepositoryException) =>
+          publish(DiscoveryRequestEvent(DiscoveryStateRequestEvent.ERROR_HTTP_REQUEST))
+          throw SWDiscoveryException(s"** RepositoryException **\ncon:$con"+"\n"+e.getMessage)
+        case Failure(e) =>
+          throw SWDiscoveryException(s"** Unknown error **"+"\n"+e.getMessage)
+       }
     }
   }
 }
