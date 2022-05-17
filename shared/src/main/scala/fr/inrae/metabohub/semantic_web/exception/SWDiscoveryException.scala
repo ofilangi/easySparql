b5df59382bac1c8d76fa5a3d6be221e72506f862
Olivier Filangi <olivier.filangi@gmail.com>
2022-05-17 16:39:22 +0200

    feat: export exception to js

--- shared/src/main/scala/fr/inrae/metabohub/semantic_web/exception/SWDiscoveryException.scala
+++ shared/src/main/scala/fr/inrae/metabohub/semantic_web/exception/SWDiscoveryException.scala
@@ -1,4 +1,11 @@
 package fr.inrae.metabohub.semantic_web.exception
 
-final case class SWDiscoveryException(private val message: String = "",
-                                      private val cause: Throwable = None.orNull) extends Exception(message,cause)
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+
+@JSExportTopLevel(name="SWDiscoveryException")
+final case class SWDiscoveryException(private val messageInt: String = "",
+                                      private val cause: Throwable = None.orNull) extends Exception(messageInt,cause) {
+  @JSExport
+  def message : String =  messageInt
+
+}
